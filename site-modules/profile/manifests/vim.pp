139fa130975ece13b4bc8cca858709a0a90b6937
John Arundel <john@bitfieldconsulting.com>
2017-07-18 15:58:55 +0100

    Add Vim profile

---
+++ site-modules/profile/manifests/vim.pp
@@ -0,0 +1,45 @@
+# Configure a sensible version of Vim
+class profile::vim {
+  ensure_packages([ 'vim-enhanced',
+                    'ack'])
+
+  file { '/bin/vi':
+    ensure => link,
+    target => '/bin/vim',
+  }
+
+  file { '/usr/share/vim/vimfiles/autoload/pathogen.vim':
+    source => 'puppet:///modules/profile/pathogen.vim',
+  }
+
+  file { '/usr/share/vim/vimfiles/bundle':
+    ensure => directory,
+  }
+
+  exec { '/usr/bin/git clone https://github.com/rodjek/vim-puppet.git vim-puppet':
+    cwd     => '/usr/share/vim/vimfiles/bundle',
+    creates => '/usr/share/vim/vimfiles/bundle/vim-puppet',
+  }
+
+  file { '/etc/vimrc':
+    source => 'puppet:///modules/profile/vimrc',
+  }
+
+  vcsrepo { '/usr/share/vim/vimfiles/bundle/ctrlp.vim':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/ctrlpvim/ctrlp.vim.git',
+  }
+
+  vcsrepo { '/usr/share/vim/vimfiles/bundle/ack.vim':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/mileszs/ack.vim.git',
+  }
+
+  vcsrepo { '/usr/share/vim/vimfiles/bundle/nerdtree':
+    ensure   => present,
+    provider => git,
+    source   => 'https://github.com/scrooloose/nerdtree.git',
+  }
+}
